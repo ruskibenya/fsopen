7ff8617842f8e2f50a89f0b56c2fddc07eebdc96
Benjamin Aronov <ruskibear31@gmail.com>
2019-11-28 15:40:14 +0200

    part1 done

---
+++ part1/part1/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
